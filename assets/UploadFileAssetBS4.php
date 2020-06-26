6fa272902b1e31030c8f2f220d310a19ac91d4e9
doris-adv <67466749+doris-adv@users.noreply.github.com>
2020-06-26 15:54:18 +0300

    Create UploadFileAssetBS4.php

---
+++ assets/UploadFileAssetBS4.php
@@ -0,0 +1,30 @@
+<?php
+namespace doris-adv\uploadFile\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * FileUploadAsset
+ */
+class FileUploadAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/blueimp-file-upload';
+    public $css = [
+        'css/jquery.fileupload.css'
+    ];
+    public $js = [
+        'js/vendor/jquery.ui.widget.js',
+        'js/jquery.iframe-transport.js',
+        'js/jquery.fileupload.js'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+    public $publishOptions = [
+        'except' => [
+            'server/*',
+            'test'
+        ],
+    ];
+}
